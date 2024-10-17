2b3fd9a6c9b61bf8045f41d88041962d0283e96a
pcoganwu <pcoganwu@shaw.ca>
2024-10-17 17:38:01 -0600

    Added implmentations for the interfaces

--- Stocks/Stocks.Application/Interfaces/IGenericRepository.cs
+++ Stocks/Stocks.Application/Interfaces/IGenericRepository.cs
@@ -4,9 +4,10 @@ namespace Stocks.Application.Interfaces
 {
     public interface IGenericRepository<T> where T : class
     {
-        Task<List<T>> GetEntitiesAsync<T>(Expression<Func<T, bool>>? expression = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, List<string>? includes = null);
-        Task<T> GetEntityAsync<T>(Expression<Func<T, bool>>? expression = null, List<string>? includes = null);
+        Task<IList<T>> GetEntitiesAsync(Expression<Func<T, bool>>? expression = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, List<string>? includes = null);
+        Task<T?> GetEntityAsync(Expression<Func<T, bool>>? expression = null, List<string>? includes = null);
         Task<T> AddEntity(T entity);
         Task<T> UpdateEntity(T entity);
+        Task<T?> DeleteEntity(Expression<Func<T, bool>>? expression = null);
     }
 }
