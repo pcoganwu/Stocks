0fd1f84c4bf0860bcf5824517aee2b46f537625e
pcoganwu <pcoganwu@shaw.ca>
2024-10-17 16:52:10 -0600

    Initial coomit

---
+++ Stocks/Stocks.Application/Interfaces/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace Stocks.Application.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<List<T>> GetEntitiesAsync<T>(Expression<Func<T, bool>>? expression = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, List<string>? includes = null);
+        Task<T> GetEntityAsync<T>(Expression<Func<T, bool>>? expression = null, List<string>? includes = null);
+        Task<T> AddEntity(T entity);
+        Task<T> UpdateEntity(T entity);
+    }
+}
---
+++ Stocks/Stocks.Application/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stocks.Application.Interfaces
+{
+    public interface IUnitOfWork
+    {
+       // IGenericRepository<Account> Accounts { get; }
+    }
+}
