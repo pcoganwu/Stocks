dea3d47743f7f894fe24b904047e06297c406299
pcoganwu <pcoganwu@shaw.ca>
2024-10-17 15:30:43 -0600

    Extended the ApplicationUser and created the initial migration

--- Stocks/Stocks.Infrastructure/Data/ApplicationUser.cs
+++ Stocks/Stocks.Infrastructure/Data/ApplicationUser.cs
@@ -5,6 +5,6 @@ namespace Stocks.Infrastructre.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public DateTime DateJoined { get; set; }
     }
-
 }
